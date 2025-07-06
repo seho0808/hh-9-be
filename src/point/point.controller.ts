7682228f08ebe61dde058c07bd80584d59c04b73
Seho Lee <seho0808@vt.edu>
2025-07-06 16:28:40 +0900

    style: GET point 컨트롤러 파라미터 ids => id 오타 수정
    - 이전 커밋들에서 실수로 오타낸 것 같음.

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -15,8 +15,8 @@ export class PointController {
    * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
    */
   @Get(':id')
-  async point(@Param('id') ids): Promise<UserPoint> {
-    const userId = Number.parseInt(ids);
+  async point(@Param('id') id): Promise<UserPoint> {
+    const userId = Number.parseInt(id);
     return { id: userId, point: 0, updateMillis: Date.now() };
   }
 
